64819c8a5a524e1c13b07ad7166a24451d6d6446
raam9431 <118127013+raam9431@users.noreply.github.com>
2025-02-24 22:16:21 +0530

    1st part

---
+++ script.js
@@ -0,0 +1,14 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate: 360,
+//     duration: 2,
+//     backgroundColor:"blue",
+//     Delay: 1
+// })
+gsap.from("#box",{
+    x:1000,
+    rotate: 360,
+    duration: 2,
+    backgroundColor:"blue",
+    Delay: 1
+})
