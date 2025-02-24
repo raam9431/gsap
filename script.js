6fbf9424e188292200f2596824a6466239c136c4
raam9431 <118127013+raam9431@users.noreply.github.com>
2025-02-25 01:50:40 +0530

    2nd part

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-// gsap.to("#box",{
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:100,
+    rotate: 360,
+    duration: 2,
+    scale:0.5,
+})
+tl.to("#box2",{
+    x:100,
+    rotate: 360,
+    duration: 2,
+    scale:0.5,
+})
+tl.to("#box3",{
+    x:100,
+    rotate: 360,
+    duration: 2,
+    scale:0.5,
+})
+tl.to("#box4",{
+    x:100,
+    rotate: 360,
+    duration: 2,
+    scale:0.5,
+})
+// gsap.from("#box",{
 //     x:1000,
 //     rotate: 360,
 //     duration: 2,
 //     backgroundColor:"blue",
 //     Delay: 1
 // })
-gsap.from("#box",{
-    x:1000,
-    rotate: 360,
-    duration: 2,
-    backgroundColor:"blue",
-    Delay: 1
-})
